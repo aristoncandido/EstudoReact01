8e7854a6bed7e60c5f2966483d4dc6ea68da04f7
Ariston <ariston.cdsf@gmail.com>
2023-02-15 15:10:36 -0300

    estudo react part 1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const user = {
+
+    nome: 'aris',
+    idade: 19
+}
+
+
+const elemento = (
+    <div>
+        <h1>Olá Mundo, bem vindo {user.nome} <br></br>
+             eu sei que vc tem {user.idade} anos
+        </h1>
+    </div>
+)
+
+
+
+
+function tick(){
+
+       
+       const nome = 'Ariston'
+       const element = (
+
+                
+          <div>
+           
+                 <h1>
+                    Salve {nome}
+                 </h1>
+                  
+                  <h2>
+                        Agora é  {new Date().toLocaleTimeString()} .
+                  </h2>
+
+                   
+
+          </div>
+
+                
+
+       )
+
+ 
+       ReactDOM.render(element, document.getElementById('root'))
+
+
+
+}
+
+
+
+setInterval(tick,1000);
+
+
+
+
+
+
+
+
+
+ReactDOM.render(
+    elemento, document.getElementById("root")
+)
